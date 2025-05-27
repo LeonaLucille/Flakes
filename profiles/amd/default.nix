e1abff3b0592750538b2c04db90b60f79e4ea6a0
Leona Techel <me@leonalucille.org>
2025-05-27 10:16:52 +0200

    rm amd

--- profiles/amd/default.nix
+++ profiles/amd/default.nix
@@ -1,9 +1,7 @@
 {host, ...}: {
   imports = [
     ../../machines/${host}
-    ../../modules/drivers/amd.nix
     ../../modules/core
   ];
   # Enable GPU Drivers
-  drivers.amdgpu.enable = true;
 }
