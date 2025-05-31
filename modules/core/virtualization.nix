f2b7b0cfba64fdde07508ef9ada45089cb092949
Leona Techel <me@leonalucille.org>
2025-05-31 15:25:54 +0200

    added first rev of Lab with K8s

--- modules/core/virtualization.nix
+++ modules/core/virtualization.nix
@@ -1,7 +1,21 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   # Only enable either docker or podman -- Not both
   virtualisation = {
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        ovmf = {
+          enable = true;
+          packages = [pkgs.OVMFFull.fd];
+        };
+        swtpm.enable = true;
+      };
+    };
     docker.enable = false;
     podman.enable = true;
   };
@@ -9,7 +23,16 @@
     virt-manager.enable = true;
   };
   environment.systemPackages = with pkgs; [
-     virt-viewer # View Virtual Machines
+    virt-viewer # View Virtual Machines
+    swtpm
   ];
-}
+  environment.etc = {
+    "ovmf/edk2-x86_64-secure-code.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+    };
 
+    "ovmf/edk2-i386-vars.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
+    };
+  };
+}
